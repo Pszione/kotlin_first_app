ed2cec177366ba4710824026303e2a7b70baaa12
Pszione <oused.games@gmail.com>
2021-12-22 17:39:16 -0300

    Kotlin - Console and Conditional - when() else

--- app/src/main/java/com/ousedgames/kotlin_first_app/ConsoleAndConditional.kt
+++ app/src/main/java/com/ousedgames/kotlin_first_app/ConsoleAndConditional.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     println("User inserted: $username")*/
 
     // CONDITIONAL
-    print("Please type a Number in the console: ")
+    /*print("Please type a Number in the console: ")
     val userNumber:Int = readLine()!!.toInt()
     fun isEvenOrOdd(number:Int): String {
         return if(number % 2 == 0){
@@ -17,5 +17,23 @@ fun main(args: Array<String>) {
             "odd"
         }
     }
-    println("User inserted: $userNumber | Number is ${isEvenOrOdd(userNumber)}")
+    println("User inserted: $userNumber | Number is ${isEvenOrOdd(userNumber)}")*/
+
+    // WHEN/SWITCH CONDITIONAL
+    println("Please type a day number of week (1 to 7): ")
+    val dayNumber:Int = readLine()!!.toInt()
+
+    var day:String = when(dayNumber)
+    {
+        // Kotlin lambda
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Invalid day choice!"
+    }
+    println("it's $day")
 }
